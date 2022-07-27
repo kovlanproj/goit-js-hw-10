9bca395152458e72194d5b61350f768876477029
kovlanproj <kozorizva@gmail.com>
2022-07-27 14:50:45 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  countriesList: document.querySelector('.country-list'),
+};
+
+fetchCountries('sw').then(renderCountryList);
+
+function renderCountryList(countries) {
+  let markup = '';
+
+  countries.forEach(country => {
+    markup += `<li>${country.name.common}</li>`;
+  });
+  refs.countriesList.innerHTML = markup;
+}
