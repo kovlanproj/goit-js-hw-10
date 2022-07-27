83ee1e23eec501167288e821ad7d7088420dd708
kovlanproj <104130825+kovlanproj@users.noreply.github.com>
2022-07-27 12:36:02 +0000

    Deploying to gh-pages from @ kovlanproj/goit-js-hw-10@f414d753d4832cb46c48758380bc7e6c0c415224 🚀

--- index.a75a7265.js
+++
@@ -1,2 +0,0 @@
-!function(){var n,t={countriesList:document.querySelector(".country-list")};(n="sw",fetch("https://restcountries.com/v3.1/name/".concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()}))).then((function(n){var o="";n.forEach((function(n){o+="<li>".concat(n.name.common,"</li>")})),t.countriesList.innerHTML=o}))}();
-//# sourceMappingURL=index.a75a7265.js.map
