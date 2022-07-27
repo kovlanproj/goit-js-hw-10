f414d753d4832cb46c48758380bc7e6c0c415224
kovlanproj <kozorizva@gmail.com>
2022-07-27 15:35:34 +0300

    add input

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
+  inputField: document.querySelector('#search-box'),
   countriesList: document.querySelector('.country-list'),
 };
 
-fetchCountries('sw').then(renderCountryList);
+refs.inputField.addEventListener(
+  'input',
+  debounce(onFieldInput, DEBOUNCE_DELAY)
+);
+
+function onFieldInput() {
+  fetchCountries(refs.inputField.value).then(renderCountryList);
+}
 
 function renderCountryList(countries) {
   let markup = '';
-
   countries.forEach(country => {
     markup += `<li>${country.name.common}</li>`;
   });
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -5,6 +5,7 @@ export const fetchCountries = name => {
     if (!response.ok) {
       throw new Error(response.status);
     }
+
     return response.json();
   });
 };
